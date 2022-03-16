855eb380e7e10b7336cb950e098487eff9b6e2ff
Eric Matsuno <aemedia@hotmail.com>
2022-03-16 07:43:50 -0700

    first commit

---
+++ UnityMenuSystem-master/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class MainMenu : SimpleMenu<MainMenu>
+{
+	public async void OnPlayPressed()
+	{
+		GameMenu.Show();
+	}
+
+    public async void OnOptionsPressed()
+    {
+		OptionsMenu.Show();
+	}
+
+	public async override void OnBackPressed()
+	{
+		Application.Quit();
+	}
+}
